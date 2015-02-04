181e3d32044bae5e63e2ab95b467cc25a56127c3
Jose Alves <a.sousajose@gmail.com>
2015-02-04 02:36:16 -0300

    adicao bootstrap e controler contatos

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,34 +1,7 @@
 angular.module('contatooh')
 .controller('ContatosController',
-    function($scope){
-        $scope.contatos = [
-            {
-            "_id": 1,
-            "nome": "Contato Angular 1",
-            "email": "cont1@empresa.com.br"
-        },
-            {
-            "_id": 2,
-            "nome": "Contato Angular 2",
-            "email": "cont2@empresa.com.br"
-        },
-
-            {
-            "_id": 3,
-            "nome": "Contato Angular 3",
-            "email": "cont3@empresa.com.br"
-        },{
-            "_id": 4,
-            "nome": "Contato Angular 4",
-            "email": "cont4@empresa.com.br"
-        },
-            {
-            "_id": 5,
-            "nome": "Contato Angular 5",
-            "email": "cont5@empresa.com.br"
-        }
-        ];
-        $scope.filtro='';
+    function($scope,$http){
+       var promise = $http({method: 'GET',url:'/contatos'});
 
         $scope.incrementa = function(){
             $scope.total++;
